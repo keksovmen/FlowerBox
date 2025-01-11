043784aaee6d2c6cce7913cb6749a85f0a6c99fa
Keksovmen <Keksovmen@yandex.ru>
2025-01-11 22:06:03 +0300

    added DF mini player and modified a bit

---
+++ components/DFRobotDFPlayerMini/serial.h
@@ -0,0 +1,26 @@
+#ifndef __UART_H__
+#define __UART_H__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+void serial_begin(int baudrate, int txd, int rxd);
+void serial_end(void);
+int serial_available(void);
+uint8_t serial_read(void);
+uint8_t serial_peek(void);
+void serial_print(char *ch);
+void serial_println(char *ch);
+void serial_write(uint8_t ch);
+void serial_write_buffer(uint8_t *ch, int len);
+void serial_flash();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif
