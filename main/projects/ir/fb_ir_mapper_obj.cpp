a0d69d890b6df9ead29306bdae85e8ab0b5ea006
Keksovmen <Keksovmen@yandex.ru>
2026-02-19 13:58:29 +0300

    added IR project MQTT settings

--- main/projects/ir/fb_ir_mapper_obj.cpp
+++ main/projects/ir/fb_ir_mapper_obj.cpp
@@ -12,9 +12,46 @@ using namespace project;
 
 
 
+static void _initMqtt()
+{
+	auto* mqttId = new box::PropertyInt("Mqtt ID", "Will apply after reboot",
+		box::Tid::PROPERTY_GENERAL,
+		[](int val){
+			settings::setMqttId(val);
+			return true;
+		}, settings::getMqttId(), 0, 255
+	);
+	getBox().addProperty(std::unique_ptr<box::PropertyIface>(mqttId));
+	getBox().addPropertyDependency(mqttId->getId());
+
+	auto* mqttIp = new box::PropertyString("Mqtt IP", "Will apply after reboot",
+		box::Tid::PROPERTY_GENERAL,
+		[](std::string val){
+			settings::setIp(val);
+			return true;
+		}, settings::getIp()
+	);
+	getBox().addProperty(std::unique_ptr<box::PropertyIface>(mqttIp));
+	getBox().addPropertyDependency(mqttIp->getId());
+
+	auto* mqttPort = new box::PropertyInt("Mqtt port", "Will apply after reboot",
+		box::Tid::PROPERTY_GENERAL,
+		[](int val){
+			// getHwMqttClient().setPause(!static_cast<bool>(val));
+			settings::setPort(val);
+			return true;
+		}, settings::getPort(), 0, (1 << 16) - 1
+	);
+	getBox().addProperty(std::unique_ptr<box::PropertyIface>(mqttPort));
+	getBox().addPropertyDependency(mqttPort->getId());
+}
+
+
+
 void project::initMaperObjs()
 {
 	util::createAndRegisterDefaultBoxProperties();
+	_initMqtt();
 }
 
 int project::mapBoxSensorIdToAddres(int id)
